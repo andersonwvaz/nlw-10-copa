b1800d40042e3c7e136f0203a54513e992f18fd0
Anderson Wilchen Vaz <andersonwvaz@gmail.com>
2022-11-06 09:45:07 -0300

    :tada: initial commit

---
+++ mainn.js
@@ -0,0 +1,51 @@
+function createGame (player1, hour, player2) {
+  return `
+  <li><!--Itens da lista-->
+                        <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+                        <strong>${hour}</strong>
+                        <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
+                    </li>
+  `
+}
+let delay = -0.4
+function creatCard(date, day, games) {
+  delay = delay + 0.4;
+  return `
+  <div class="card" style="animation-delay: ${delay}s">
+                <h2>${date} <span>${day}</span></h2>
+                <ul> <!--Lista não ordenada-->
+                ${games}
+                </ul>
+            </div>
+  `
+}
+
+document.querySelector("#cards").innerHTML =
+            creatCard(
+              "21/11",
+              "segunda",
+              createGame("brazil", "08:00", "cameroon") +
+                createGame("hungary", "13:00", "argentina") +
+                createGame("colombia", "20:00", "japan")
+            ) +
+            creatCard(
+              "24/11",
+              "quinta",
+              createGame("switzerland", "07:00", "cameroon") +
+                createGame("portugal", "13:00", "ghana") +
+                createGame("brazil", "16:00", "serbia")
+            ) +
+            creatCard(
+              "28/11",
+              "segunda",
+              createGame("south-korea", "10:00", "ghana") +
+                createGame("brazil", "13:00", "switzerland") +
+                createGame("portugal", "16:00", "uruguay")
+            ) + 
+            creatCard(
+              "28/11",
+              "segunda",
+              createGame("south-korea", "10:00", "ghana") +
+                createGame("brazil", "13:00", "switzerland") +
+                createGame("portugal", "16:00", "uruguay")
+            )
